7957d2fe818211408b1a759046a13abd4112d750
Lloyd Weehuizen <lweehuizen@roblox.com>
2021-01-28 10:26:52 +1300

    Fix run when no network present

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,6 +5,11 @@ COLOR_NONE='\033[0m'
 ENV_KEYS=`echo $INPUT_ENV | jq -jr '. | to_entries[] | "-e \(.key) "'`
 NETWORK=`docker network ls | grep github_network | awk '{ print $2 }'`
 RUN=`echo "$INPUT_RUN" | sed -ze 's/[ \n]*$//' -e 's/\\n/;/g'`
+NETWORK_OPTIONS=""
+if [ ! -z $NETWORK ]; then
+    NETWORK_OPTIONS="--network $NETWORK"
+fi
 
-echo -e $COLOR_LB docker run $INPUT_OPTIONS --network $NETWORK $ENV_KEYS -v $GITHUB_WORKSPACE:/workspace -w /workspace --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "$RUN" $COLOR_NONE
-exec docker run $INPUT_OPTIONS --network $NETWORK $ENV_KEYS -v $GITHUB_WORKSPACE:/workspace -w /workspace --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "$RUN"
+DOCKER_CMD="docker run $INPUT_OPTIONS $NETWORK_OPTIONS $ENV_KEYS -v $GITHUB_WORKSPACE:/workspace -w /workspace --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c"
+echo -e $COLOR_LB $DOCKER_CMD "$RUN" $COLOR_NONE
+exec $DOCKER_CMD "$RUN"
